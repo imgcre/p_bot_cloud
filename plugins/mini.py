fa1344475b64434569efc64542ee9c6b952ff7c3
na_pluss <imgcre@outlook.com>
2025-12-17 21:49:06 +0800

    测试plugin端点

---
+++ plugins/mini.py
@@ -0,0 +1,30 @@
+
+from mirai.asgi import ASGI
+from starlette.requests import Request
+from starlette.responses import JSONResponse
+from plugin import Plugin, autorun, route
+
+@route('mini')
+class Mini(Plugin):
+
+    async def test_endpoint(request: Request):
+        # 获取 JSON 数据
+        data: dict[str, str] = await request.json()
+        
+        # 或者手动解析
+        # body = await request.body()
+        # if body:
+        #     data = json.loads(body)
+        
+        # 获取特定字段
+        name = data.get("name", "未知")
+        
+        return JSONResponse({
+            "status": "ok",
+            "name": name
+        })
+
+    @autorun
+    async def startup(self):
+        asgi = ASGI()
+        asgi.add_route('/mini-test', self.test_endpoint, ['POST'])
